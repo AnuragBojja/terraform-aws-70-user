ff1e898d7f8e254ac0a9f1b5c28ec4beda800c5c
AnuragBojja <anuragbojja19@gmail.com>
2026-04-21 00:07:25 -0500

    70-user

--- provider.tf
+++ provider.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-  backend "s3" {
-    bucket = "roboshop-terraform-dev-daws-test"
-    key    = "remote-state-roboshop-dev-70-user"
-    region = "us-east-1"
-    use_lockfile = true
-    encrypt = true
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 6.0"
+#     }
+#   }
+#   backend "s3" {
+#     bucket = "roboshop-terraform-dev-daws-test"
+#     key    = "remote-state-roboshop-dev-70-user"
+#     region = "us-east-1"
+#     use_lockfile = true
+#     encrypt = true
+#   }
+# }
 
